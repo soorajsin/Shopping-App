18ed738122b62f21bc167a6b07e5cc9e33889b94
Sooraj <soorajsingh7505@gmail.com>
2023-10-17 17:10:01 +0530

    upload

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,31 @@ import config from "../../config";
 const Navbar = () => {
   const url = config.backendURL;
   const history = useNavigate();
-  const { userdata } = useContext(ContextNavigate);
+  const { userdata, setUserData } = useContext(ContextNavigate);
+
+  const navbarAuth = async () => {
+    const token = await localStorage.getItem("userDataToken");
+
+    const data = await fetch(`${url}/validUser`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    });
+
+    const res = await data.json();
+
+    if (res.status === 210) {
+      setUserData(res);
+    } else {
+      history("*");
+    }
+  };
+
+  useEffect(() => {
+    navbarAuth();
+  });
 
   const signOut = async () => {
     const token = await localStorage.getItem("userDataToken");
@@ -32,84 +56,110 @@ const Navbar = () => {
     }
   };
 
-  useEffect(() => {
-    if (userdata) {
-      console.log("user find");
-    } else if (!navigator.onLine) {
-      window.location.reload();
-    }
-  }, [userdata]);
-
-  // Conditional rendering based on user authorization
-  const renderAuthorizedNavbar = () => {
+  const ifUserValid = () => {
     return (
-      <div className="container">
-        <div className="tab1">
-          <NavLink to={"/"}>
-            <img src={SoorajLogo} alt="logo" />
-          </NavLink>
-        </div>
-        <div className="tab">
-          <input type="text" placeholder="Search Product here..." />
-          <button>Search</button>
-        </div>
-        <div className="tab">
-          <NavLink to={"/login"} className={"tabbutton"}>
-            Login
-          </NavLink>
-        </div>
-        <div className="tab">
-          <NavLink to={"/product"} className={"tabbutton"}>
-            <i className="fa-solid fa-cart-shopping"></i>
-          </NavLink>
-        </div>
-        <div className="tabFinished">
-          <NavLink className={"avatar"}>
-            <Avatar className="avatar-main">
-              {userdata ? userdata.getData.email.charAt(0).toUpperCase() : ""}
-            </Avatar>
-            <div id="avatar-manu">
-              <div className="manuEmail">{userdata.getData.email}</div>
-              <div className="manu">
-                <NavLink to={"/"} className="manuItem">
-                  Home
-                </NavLink>
-                <NavLink to={"/product"} className="manuItem">
-                  Product
-                </NavLink>
-                <NavLink to={"/login"} className="manuItem">
-                  Login
-                </NavLink>
-                <NavLink onClick={signOut} className="manuItem">
-                  Sign Out
-                </NavLink>
+      <>
+        <div className="container">
+          <div className="tab1">
+            <NavLink to={"/"}>
+              <img src={SoorajLogo} alt="logo" />
+            </NavLink>
+          </div>
+          <div className="tab">
+            <input type="text" placeholder="Search Product here..." />
+            <button>Search</button>
+          </div>
+          <div className="tab">
+            <NavLink to={"/login"} className={"tabbutton"}>
+              Login
+            </NavLink>
+          </div>
+          <div className="tab">
+            <NavLink to={"/product"} className={"tabbutton"}>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </NavLink>
+          </div>
+          <div className="tabFinished">
+            <NavLink className={"avatar"}>
+              <Avatar className="avatar-main">
+                {userdata ? userdata.getData.email.charAt(0).toUpperCase() : ""}
+              </Avatar>
+              <div id="avatar-manu">
+                <div className="manuEmail">{userdata.getData.email}</div>
+                <div className="manu">
+                  <NavLink to={"/"} className="manuItem">
+                    Home
+                  </NavLink>
+                  <NavLink to={"/product"} className="manuItem">
+                    Product
+                  </NavLink>
+                  <NavLink to={"/login"} className="manuItem">
+                    Login
+                  </NavLink>
+                  <NavLink onClick={signOut} className="manuItem">
+                    Sign Out
+                  </NavLink>
+                </div>
               </div>
-            </div>
-          </NavLink>
+            </NavLink>
+          </div>
         </div>
-      </div>
+        ;
+      </>
     );
   };
 
-  // Conditional rendering for non-authorized users (e.g., showing the Login tab only)
-  const renderNonAuthorizedNavbar = () => {
+  const elseNotValid = () => {
     return (
-      <div className="container">
-        <div className="tab">
-          <NavLink to={"/login"} className={"tabbutton"}>
-            Login
-          </NavLink>
+      <>
+        <div className="container">
+          <div className="tab1">
+            <NavLink to={"/"}>
+              <img src={SoorajLogo} alt="logo" />
+            </NavLink>
+          </div>
+          <div className="tab">
+            <input type="text" placeholder="Search Product here..." />
+            <button>Search</button>
+          </div>
+          <div className="tab">
+            <NavLink to={"/login"} className={"tabbutton"}>
+              Login
+            </NavLink>
+          </div>
+          <div className="tab">
+            <NavLink to={"/product"} className={"tabbutton"}>
+              <i className="fa-solid fa-cart-shopping"></i>
+            </NavLink>
+          </div>
+          <div className="tabFinished">
+            <NavLink className={"avatar"}>
+              <Avatar className="avatar-main"></Avatar>
+              <div id="avatar-manu">
+                {/* <div className="manuEmail">{userdata.getData.email}</div> */}
+                <div className="manu">
+                  <NavLink to={"/"} className="manuItem">
+                    Home
+                  </NavLink>
+                  <NavLink to={"/product"} className="manuItem">
+                    Product
+                  </NavLink>
+                  <NavLink to={"/login"} className="manuItem">
+                    Login
+                  </NavLink>
+                </div>
+              </div>
+            </NavLink>
+          </div>
         </div>
-      </div>
+      </>
     );
   };
 
   return (
     <div className="navbar">
       <AppBar>
-        <Toolbar>
-          {userdata ? renderAuthorizedNavbar() : renderNonAuthorizedNavbar()}
-        </Toolbar>
+        <Toolbar>{userdata ? ifUserValid() : elseNotValid()}</Toolbar>
       </AppBar>
     </div>
   );
